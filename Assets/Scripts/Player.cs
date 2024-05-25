4ed81928fc94a1af2646b722f0323a42d524e721
onur kaya <onutkaya@gmail.com>
2024-05-26 00:47:44 +0300

    ilk commit
    Karakter haraket  eklendi

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 7f;  
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0); 
+        if(Input.GetKey(KeyCode.W)){
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+
+        }
+        Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+        inputVector = inputVector.normalized;
+        transform.position += moveDir*Time.deltaTime*moveSpeed;
+
+
+    }
+}
