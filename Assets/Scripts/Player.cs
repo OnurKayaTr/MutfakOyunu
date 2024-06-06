6ca5cb215697dd14578868ab605176936e596a47
onur kaya <onutkaya@gmail.com>
2024-06-06 05:01:37 +0300

    Tag eklnd
    layermask - raycast tamam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,42 @@ public class Player : MonoBehaviour
 
    [SerializeField] private float moveSpeed = 7f;
    [SerializeField] private PlayerInputs gameInput;
-
-    
+    [SerializeField] private LayerMask counterLayerMask;
+   private Vector3 lastInteractDir;
 
    private void Update()
     {
-       Vector2 inputVector = gameInput.GetmovementVectorNormalized();
+        HandelMovement();
+        HandleOInteractions();
+    }
+
+
+
+    private void HandleOInteractions()
+    {
+        float interactDistance = 2f;
+        Vector2 inputVector = gameInput.GetmovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero) { 
+        lastInteractDir = moveDir;
+        }
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+           if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+       
+    }
+    private void HandelMovement()
+    {
+        Vector2 inputVector = gameInput.GetmovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-       float moveDistance = moveSpeed * Time.deltaTime;
+        float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = .7f;
         float playerHeight = 2f;
 
@@ -45,13 +72,18 @@ public class Player : MonoBehaviour
 
             }
         }
-            if (canMove)
-            {
-                transform.position += moveDir * moveDistance;
-            }
-            // transform.position += moveDir * Time.deltaTime * moveSpeed;
-            float rotateSpeed = 10f;
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        // transform.position += moveDir * Time.deltaTime * moveSpeed;
+        float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
     }
+
+
+
+
+
 }
